c9e7f4ae0025195e04f042e61c8b0ca53960e828
Matías Pereyra <47004530+mpereyra-mi@users.noreply.github.com>
2021-01-21 17:17:34 -0300

    Update Teams connector send function parametrizing timeout variables
    Hello, thanks for you project it is being useful for me, I am getting timeout issues from time to time and maybe parametrizing those values will give us more flexibility trying to avoid this issue

--- src/TeamsConnector.php
+++ src/TeamsConnector.php
@@ -18,9 +18,11 @@ class TeamsConnector
      * Sends card message as POST request
      *
      * @param  TeamsConnectorInterface $card
+     * @param  int $curlOptTimeout by default = 10
+     * @param  int $curlOptConnectTimeout by default = 3
      * @throws Exception
      */
-    public function send(TeamsConnectorInterface $card)
+    public function send(TeamsConnectorInterface $card, int $curlOptTimeout = 10, int $curlOptConnectTimeout = 3)
     {
         $json = json_encode($card->getMessage());
 
@@ -28,8 +30,8 @@ class TeamsConnector
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
         curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $curlOptTimeout);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $curlOptConnectTimeout);
         curl_setopt($ch, CURLOPT_HTTPHEADER, [
             'Content-Type: application/json',
             'Content-Length: ' . strlen($json)
